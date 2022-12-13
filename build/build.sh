63c634221080dbca7849ec5f5ef07bdedf807f9e
Martin Studer <martin.studer@medi.ch>
2022-12-13 13:31:21 +0100

    added skin and some plugins

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-docker build . --pull --target ilias -t learn-medi-docker-ilias/ilias:7.16-1
-docker build . --pull --target nginx -t learn-medi-docker-ilias/nginx:7.16-1
-docker build . --pull --target cron -t learn-medi-docker-ilias/cron:7.16-1
-docker build . --pull --target ilserver -t learn-medi-docker-ilias/ilserver:7.16-1
+docker build . --pull --target ilias -t learn-medi-docker-ilias/ilias:7.16-2
+docker build . --pull --target nginx -t learn-medi-docker-ilias/nginx:7.16-2
+docker build . --pull --target cron -t learn-medi-docker-ilias/cron:7.16-2
+docker build . --pull --target ilserver -t learn-medi-docker-ilias/ilserver:7.16-2
