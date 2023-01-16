2229420b7ac417c80dbacd4d008222dd9e84ee9c
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2023-01-16 12:57:54 +0100

    fix/live-voting-short-url

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,4 @@
-
 #!/bin/bash
-docker build . --target ilias -t learn-medi-docker-ilias/ilias:2023-01-05-1
-docker build . --target nginx -t learn-medi-docker-ilias/nginx:2023-01-05-1
-docker build . --target ilserver -t learn-medi-docker-ilias/ilserver:2023-01-05-1
+docker build . --pull --target ilias -t learn-medi-docker-ilias/ilias:2023-01-16-1
+docker build . --pull --target nginx -t learn-medi-docker-ilias/nginx:2023-01-16-1
+docker build . --pull --target ilserver -t learn-medi-docker-ilias/ilserver:2023-01-16-1
