0b4313a66c4d5cb559734576ea59d07b4db78731
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2023-01-19 22:42:52 +0100

    version 2023-01-19-1

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build . --pull --target ilias -t learn-medi-docker-ilias/ilias:2023-01-18-1
-docker build . --pull --target nginx -t learn-medi-docker-ilias/nginx:2023-01-18-1
-docker build . --pull --target ilserver -t learn-medi-docker-ilias/ilserver:2023-01-18-1
+docker build . --pull --target ilias -t learn-medi-docker-ilias/ilias:2023-01-19-1
+docker build . --pull --target nginx -t learn-medi-docker-ilias/nginx:2023-01-19-1
+docker build . --pull --target ilserver -t learn-medi-docker-ilias/ilserver:2023-01-19-1
