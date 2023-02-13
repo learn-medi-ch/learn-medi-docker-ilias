0a04d4e4c81489f17b0fe159e574d9c049e5359f
Martin Studer <martin.studer@medi.ch>
2023-02-13 14:01:52 +0100

    2023-02-13-1

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build . --pull --target ilias -t learn-medi-docker-ilias/ilias:2023-02-1-1
-docker build . --pull --target nginx -t learn-medi-docker-ilias/nginx:2023-02-1-1
+docker build . --pull --target ilias -t learn-medi-docker-ilias/ilias:2023-02-13-1
+docker build . --pull --target nginx -t learn-medi-docker-ilias/nginx:2023-02-13-1
