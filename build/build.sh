8b94639d1b9188dc694c5286c1fef266e72e4462
Martin Studer <martin@fluxlabs.ch>
2023-05-09 17:55:23 +0200

    ...

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build . --pull --target ilias -t learn-medi-docker-ilias/ilias:2023-05-09-2
-docker build . --pull --target nginx -t learn-medi-docker-ilias/nginx:2023-05-09-2
+docker build . --pull --target ilias -t learn-medi-docker-ilias/ilias:2023-05-09-3
+docker build . --pull --target nginx -t learn-medi-docker-ilias/nginx:2023-05-09-3
